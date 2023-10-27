ca2b8040ecbd23344f7c2fa00e2fdb94ad91f1d2
Tanushanth <79293445+Tanushanth@users.noreply.github.com>
2023-10-27 13:29:28 -0400

    Fixing issue with carrying 0s

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from essential_matrix_computation import essential_matrix_computation
 from featuretracking import feature_tracking
 from corner_detection_fast import extract_features
 
-videopath = "C:/Users/tanus/Downloads/20230605_163120_Trim.mp4"
+videopath = "C:/Users/tanus/Downloads/20230605_163120_Trim2.mp4"
 cap = cv2.VideoCapture(videopath)
 if not cap.isOpened():
     print("Access error")
@@ -22,12 +22,13 @@ frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 # Store one frame behinds info
 prevFrameCorners = []
 prevFrame = ()
-rotationArr = np.zeros(shape=(3, 3))
+rotationArr = np.ones(shape=(3, 3))
 translationArr = np.empty(shape=(3, 1))
 focal = 718.8560
 pp = (607.1928, 185.2157)
 
-current_point = np.array([0, 0, 0])
+current_point = np.array([1, 1, 1])
+
 path = [current_point]
 colours = [(0, 0, 0)]
 step = 1
@@ -67,24 +68,30 @@ for i in tqdm(range(int(frame_count))):
     idx += 1
 
     # Plotting points after rotation and translation.
-    rotated_point = np.matmul(rotationArr, current_point).reshape(3, 1)
+    rotated_point = np.matmul(rotationArr, current_point).reshape(3,1)
+    print(rotated_point)
+
 
     translated_point = np.multiply(translationArr, rotated_point)
     current_point = translated_point
     path.append(current_point)
 
-    
     # Distinguishing points in time by colour gradient.
     new_colour = (cmp + step for cmp in colours[idx - 1])
     colours.append(new_colour)
 
+path = path[:-1]
 x = [point[0] for point in path]
 y = [point[1] for point in path]
 z = [point[2] for point in path]
 
+flattened_x = np.array([item for sublist in x for item in (sublist if isinstance(sublist, (list, np.ndarray)) else [sublist])])
+flattened_y = np.array([item for sublist in y for item in (sublist if isinstance(sublist, (list, np.ndarray)) else [sublist])])
+
+
 # Plotting points with matplotlib in 2D.
 plt.title("Robot Path")
-plt.plot(x, y)
+plt.plot(flattened_x, flattened_y)
 plt.show()
 
 # Plotting points with plotly in 3D.
